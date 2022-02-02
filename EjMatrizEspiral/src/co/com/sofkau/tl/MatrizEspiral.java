3c98f82262bcd831209a04ccf08e23e9a097bbbc
Ignacio Armand-ugón <Matias4371@gmail.com>
2022-02-02 13:15:06 -0300

    Creados metodo main, crearMatriz y solicitarTaÃnaño

--- EjMatrizEspiral/src/co/com/sofkau/tl/MatrizEspiral.java
+++ EjMatrizEspiral/src/co/com/sofkau/tl/MatrizEspiral.java
@@ -1,6 +1,8 @@
 package EjMatrizEspiral.src.co.com.sofkau.tl;
 
 
+import java.util.Scanner;
+
 public class MatrizEspiral {
     // TODO: 2/02/2022 método para recorrer fila izq-der
     // TODO: 2/02/2022 método para recorrer fila der-izq
@@ -9,5 +11,22 @@ public class MatrizEspiral {
     // TODO: 2/02/2022 método para solicitar tamaño de matriz a llenar <-- Nuestra tarea
     // TODO: 2/02/2022 método para imprimir la matriz
     // TODO: 2/02/2022 método para integrar todas las funcionalidades anteriores por medio de un menú que se comunique con el usuario
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] crearMatriz(int x, int y){
+        int matriz[][] = new int[x][y];
+        return matriz;
+    }
+    public int[][] solicitarTamaño(int x, int y){
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese valor x");
+        x = teclado.nextInt();
+        System.out.println("Ingrese valor y");
+        y = teclado.nextInt();
+        return crearMatriz(x, y);
+    }
 
 }
